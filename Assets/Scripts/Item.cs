cae8736d8c3d216b22821b3e0720a919b841bab8
SanaaKamau <98336407+SanaaKamau@users.noreply.github.com>
2025-11-11 17:29:22 -0500

    initial ingredient and recipe json setup

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,20 +2,29 @@ using System;
 
 public class Item
 {
+    public int ID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string IconPath { get; set; }
     public string PrefabPath { get; set; }
 }
+[System.Serializable]
+public class RecipeIngredient
+{
+    public int IngredientID; 
+    public int Quantity;      
+}
 
 [System.Serializable]
-public class Food : Item, ICraftable
+public class Recipe : Item, IFood
 {
-     public bool CanCraft()
+    public RecipeIngredient ingredients; 
+    public bool CanCraft()
     {
         bool craftable = false;
         return craftable;
     }
+    
 
 
 }
